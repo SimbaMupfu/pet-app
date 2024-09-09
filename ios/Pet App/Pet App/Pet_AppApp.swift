883729620795e9ba5195e250d8b8f6e74a038011
SimbaMupfu <scmupfururirwa@gmail.com>
2024-09-09 22:26:33 +0200

    Initial commit

---
+++ ios/Pet App/Pet App/Pet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_AppApp.swift
+//  Pet App
+//
+//  Created by Simbarashe Mupfururirwa on 2024/09/09.
+//
+
+import SwiftUI
+
+@main
+struct Pet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
